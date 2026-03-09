c5a5d635b86f900f6daa771ae8d3d930a9900bff
Pawar-Sudharshan <pawarsudharshan47@gmail.com>
2026-03-09 23:53:21 +0530

    Neon database added with schema

---
+++ config/schema.tsx
@@ -0,0 +1,10 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: varchar({ length: 255 }).notNull(),
+    email: varchar({ length: 255 }).notNull().unique(),
+    points: integer().notNull().default(0),
+    subscription: varchar({ length: 255 }).notNull().default("free"),
+
+});
